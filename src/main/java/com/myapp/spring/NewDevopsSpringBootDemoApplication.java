57ed97ce8faf0cd4448f28312a23c041f69d8a6e
Yaseen5155 <86192513+Yaseen5155@users.noreply.github.com>
2021-07-14 13:14:03 +0530

    changes

--- src/main/java/com/myapp/spring/NewDevopsSpringBootDemoApplication.java
+++ src/main/java/com/myapp/spring/NewDevopsSpringBootDemoApplication.java
@@ -3,6 +3,9 @@ package com.myapp.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+/*
+ * some chnage made
+ */
 
 @SpringBootApplication(	)
 public class NewDevopsSpringBootDemoApplication {
